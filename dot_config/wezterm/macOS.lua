ace8443811e1d137ccbaf70f29f5f43af8990a72
Bernát Gábor <gaborjbernat@gmail.com>
2021-03-06 15:01:47 +0000

    Fix config
    Signed-off-by: Bernát Gábor <gaborjbernat@gmail.com>

--- dot_config/wezterm/macOS.lua
+++
@@ -1,7 +0,0 @@
-return {
-    default_prog = {"/usr/local/bin/pwsh", "-NoLogo"},
-    launch_menu = {
-        {args = {"htop"}}, {label = "Bash", args = {"bash"}},
-        {label = "zsh", args = {"zsh"}}, {label = "Fish", args = {"fish"}}
-    }
-}
